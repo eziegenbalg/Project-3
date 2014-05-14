6c22bd7956ad411a3531ed68ff09bf3d3852fe95
Erik Ziegenbalg <erik@Hefeweizen.(none)>
2014-05-14 14:37:16 -0700

    Initial commit with working Binary search tree. Use insert() and create_node() to add into the tree. Still working on properly painting the tree redBlack

---
+++ purple_malloc.c
@@ -0,0 +1,35 @@
+/* Main */
+
+#include "tree.h"
+#include "purple_malloc.h"
+
+void main()
+{
+ void *tmp;
+ struct node *root = create_node(tmp, 1023, tmp, 10);
+ 
+ insert(root, create_node(tmp, 1022, tmp, 11));
+ insert(root, create_node(tmp, 1021, tmp, 12));
+ insert(root, create_node(tmp, 1024, tmp, 13));
+ insert(root, create_node(tmp, 1025, tmp, 14));
+ insert(root, create_node(tmp, 1032, tmp, 15));
+ insert(root, create_node(tmp, 1026, tmp, 16));
+
+ slug_memstat(root);
+}
+
+void slug_malloc();
+void slug_free();
+
+void slug_memstat(struct node *root) 
+{
+ if(root == NULL) return;
+
+ slug_memstat(root->left);
+ printf("=== Node @ %p Color: %i ===\nAddress: %p\nLength: %i\n"
+        "Location: %p\nTimestamp: %lld\n"
+        "===========================\n", 
+        root, root->color, root->address, root->length, root->location, (long long) root->timestamp); 
+ slug_memstat(root->right);
+}
+
---
+++ tree.c
@@ -0,0 +1,171 @@
+/* This file contains the functions to implement the red-black tree
+ * All code is used directly from the main wikipedia page on Red/Black trees 
+ */
+
+#include "tree.h"
+
+void insert_case1(struct node *n);
+void insert_case2(struct node *n);
+void insert_case3(struct node *n);
+void insert_case4(struct node *n);
+void insert_case5(struct node *n);
+
+void rotate_right(struct node *n);
+void rotate_left(struct node *n);
+
+struct node *uncle(struct node *n);
+struct node *grandparent(struct node *n);
+
+struct node *insert(struct node *root, struct node *n) 
+{
+ if(root == NULL) {
+  root = n;
+  return root;
+ }
+
+ if(root->length < n->length)
+   root->right = insert(root->right, n);
+ if(root->length > n->length)
+   root->left = insert(root->left, n);
+
+ return root; 
+
+}
+
+struct node *create_node(int *address, int length, int *location, time_t timestamp)
+{
+ struct node *n = malloc(sizeof(struct node));
+ if(n == NULL) return ENOMEM;
+
+ n->address = address;
+ n->length = length;
+ n->location = location;
+ n->timestamp = timestamp;
+
+ n->parent = NULL;
+ n->left = NULL;
+ n->right = NULL;
+ n->color = RED;
+
+ return n;
+}
+
+struct node *grandparent(struct node *n)
+{
+ if ((n != NULL) && (n->parent != NULL))
+  return n->parent->parent;
+ else
+  return NULL;
+}
+ 
+struct node *uncle(struct node *n)
+{
+ struct node *g = grandparent(n);
+ if (g == NULL)
+  return NULL; // No grandparent means no uncle
+ if (n->parent == g->left)
+  return g->right;
+ else
+  return g->left;
+}
+
+void insert_case1(struct node *n)
+{
+ if (n->parent == NULL)
+  n->color = BLACK;
+ else
+  insert_case2(n);
+}
+
+void insert_case2(struct node *n)
+{
+ if (n->parent->color == BLACK)
+  return; /* Tree is still valid */
+ else;
+  insert_case3(n);
+}
+
+void insert_case3(struct node *n)
+{
+ struct node *u = uncle(n), *g;
+ 
+ if ((u != NULL) && (u->color == RED)) {
+  n->parent->color = BLACK;
+  u->color = BLACK;
+  g = grandparent(n);
+  g->color = RED;
+  insert_case1(g);
+ } else {
+  insert_case4(n);
+ }
+}
+
+void insert_case4(struct node *n)
+{
+ struct node *g = grandparent(n);
+ 
+ if ((n == n->parent->right) && (n->parent == g->left)) {
+  rotate_left(n->parent);
+ 
+ /*
+ * rotate_left can be the below because of already having *g =  grandparent(n) 
+ *
+ * struct node *saved_p=g->left, *saved_left_n=n->left;
+ * g->left=n; 
+ * n->left=saved_p;
+ * saved_p->right=saved_left_n;
+ * 
+ * and modify the parent's nodes properly
+ */
+ 
+  n = n->left; 
+ 
+ } else if ((n == n->parent->left) && (n->parent == g->right)) {
+  rotate_right(n->parent);
+ 
+ /*
+ * rotate_right can be the below to take advantage of already having *g =  grandparent(n) 
+ *
+ * struct node *saved_p=g->right, *saved_right_n=n->right;
+ * g->right=n; 
+ * n->right=saved_p;
+ * saved_p->left=saved_right_n;
+ * 
+ */
+ 
+  n = n->right; 
+ }
+ insert_case5(n);
+}
+
+void insert_case5(struct node *n)
+{
+ struct node *g = grandparent(n);
+ 
+ n->parent->color = BLACK;
+ g->color = RED;
+ if (n == n->parent->left)
+  rotate_right(g);
+ else
+  rotate_left(g);
+}
+
+void rotate_right(struct node *g)
+{
+ struct node *temp = g->left;
+
+ g->left = temp->right;
+ temp->right = g;
+}
+
+void rotate_left(struct node *g)
+{
+ struct node *temp = g->right;
+
+ g->right = temp->left;
+ temp->left = g;
+}
+
+ 
+
+
