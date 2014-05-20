c32778e020d183f9c6697c865049421c1b104c92
Jesse Cheun <jcheun@ucsc.edu>
2014-05-20 16:50:36 -0700

    Add malloc and free define

--- main.c
+++ main.c
@@ -2,15 +2,13 @@
 #include <stdio.h>
 #include "purple_malloc.h"
 
-#define FUNCTIONIZE(a,b)   a(b)
-#define STRINGIZE(a)       #a
-#define INT2STRING(i)      FUNCTIONIZE(STRINGIZE, i)
-#define FILE_POS           __FILE__ ":" INT2STRING(__LINE__)
-
+#define malloc(x)          slug_malloc(x, FILE_POS)
+#define free(x)            slug_free(x,FILE_POS)
 
 
 int main(int argc, char **argv) {
-   void *tmp1,*tmp2,*tmp3,*tmp4,*tmp5,*tmp6,*tmp7,*tmp8;
+   void *tmp,*tmp1,*tmp2,*tmp3,*tmp4,*tmp5,*tmp6,*tmp7,*tmp8;
+   tmp  = malloc(0 * sizeof(int));
    tmp1 = slug_malloc(1 * sizeof(char), FILE_POS);
    tmp2 = slug_malloc(2 *sizeof(char), FILE_POS);
    tmp3 = slug_malloc(3 * sizeof(char), FILE_POS);
@@ -19,6 +17,7 @@ int main(int argc, char **argv) {
    tmp6 = slug_malloc(6 * sizeof(char), FILE_POS);
    tmp7 = slug_malloc(7 * sizeof(char), FILE_POS);
    tmp8 = slug_malloc(8 * sizeof(char), FILE_POS);
+   free(tmp);
    slug_free(tmp1,FILE_POS);
    slug_free(tmp2,FILE_POS);
    slug_free(tmp3,FILE_POS);
--- purple_malloc.h
+++ purple_malloc.h
@@ -1,6 +1,12 @@
 #ifndef __PURPLE_MALLOC__
 #define __PURPLE_MALLOC__
 
+#define FUNCTIONIZE(a,b)   a(b)
+#define STRINGIZE(a)       #a
+#define INT2STRING(i)      FUNCTIONIZE(STRINGIZE, i)
+#define FILE_POS           __FILE__ ":" INT2STRING(__LINE__)
+
+
 #define MAX_ALLOCS 100
 
 typedef struct{
